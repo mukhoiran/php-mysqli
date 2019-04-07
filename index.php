9b50d55005483ce77b8dcb29f34a1ff17eb77ed7
mukhoiran <mkhoir.anam@gmail.com>
2019-04-07 17:03:52 +0700

    first query

--- index.php
+++ index.php
@@ -9,6 +9,15 @@ if(!$link){
   die('error happen'.mysqli_connect_error());
 }
 
+//First query
+// $query = 'CREATE DATABASE leraning123';
+//
+// if(mysqli_query($link, $query)){
+//   echo 'database create successfully';
+// }else{
+//   echo 'failed!';
+// }
+
 //close connection
 mysqli_close($link);
 ?>
